6b75f1144bd1fd938cf1a81d31b4068d1e88bf12
djfaizp <modan.mohammad@gmail.com>
2024-07-05 17:25:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-API_ID = "26482070"
-API_HASH = "28f1d67e2693e057ae4611521c555136"
+import os
 
-BOT_TOKEN = "7411202018:AAFuVrnxb0X27OTa-UOJfsMksBacVKoHaZE"
-OWNER_ID = "5128979564"
-
-MONGO_DB_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
-MUST_JOIN = "English_Spoken_with_Nitu_mam"
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
